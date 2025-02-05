0cced51664bdd488aac30f756404468753adc927
Rupali Jain <rupali.jain@simformsolutions.com>
2025-02-05 18:47:49 +0530

    Week 2

---
+++ Database/8 Week SQL Challenge/Week_2.sql
@@ -0,0 +1,282 @@
+CREATE SCHEMA pizza_runner;
+SET search_path = pizza_runner;
+
+DROP TABLE IF EXISTS runners;
+CREATE TABLE runners (
+  "runner_id" INTEGER,
+  "registration_date" DATE
+);
+INSERT INTO runners
+  ("runner_id", "registration_date")
+VALUES
+  (1, '2021-01-01'),
+  (2, '2021-01-03'),
+  (3, '2021-01-08'),
+  (4, '2021-01-15');
+
+
+DROP TABLE IF EXISTS customer_orders;
+CREATE TABLE customer_orders (
+  "order_id" INTEGER,
+  "customer_id" INTEGER,
+  "pizza_id" INTEGER,
+  "exclusions" VARCHAR(4),
+  "extras" VARCHAR(4),
+  "order_time" TIMESTAMP
+);
+
+INSERT INTO customer_orders
+  ("order_id", "customer_id", "pizza_id", "exclusions", "extras", "order_time")
+VALUES
+  ('1', '101', '1', '', '', '2020-01-01 18:05:02'),
+  ('2', '101', '1', '', '', '2020-01-01 19:00:52'),
+  ('3', '102', '1', '', '', '2020-01-02 23:51:23'),
+  ('3', '102', '2', '', NULL, '2020-01-02 23:51:23'),
+  ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
+  ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
+  ('4', '103', '2', '4', '', '2020-01-04 13:23:46'),
+  ('5', '104', '1', 'null', '1', '2020-01-08 21:00:29'),
+  ('6', '101', '2', 'null', 'null', '2020-01-08 21:03:13'),
+  ('7', '105', '2', 'null', '1', '2020-01-08 21:20:29'),
+  ('8', '102', '1', 'null', 'null', '2020-01-09 23:54:33'),
+  ('9', '103', '1', '4', '1, 5', '2020-01-10 11:22:59'),
+  ('10', '104', '1', 'null', 'null', '2020-01-11 18:34:49'),
+  ('10', '104', '1', '2, 6', '1, 4', '2020-01-11 18:34:49');
+
+
+DROP TABLE IF EXISTS runner_orders;
+CREATE TABLE runner_orders (
+  "order_id" INTEGER,
+  "runner_id" INTEGER,
+  "pickup_time" VARCHAR(19),
+  "distance" VARCHAR(7),
+  "duration" VARCHAR(10),
+  "cancellation" VARCHAR(23)
+);
+
+INSERT INTO runner_orders
+  ("order_id", "runner_id", "pickup_time", "distance", "duration", "cancellation")
+VALUES
+  ('1', '1', '2020-01-01 18:15:34', '20km', '32 minutes', ''),
+  ('2', '1', '2020-01-01 19:10:54', '20km', '27 minutes', ''),
+  ('3', '1', '2020-01-03 00:12:37', '13.4km', '20 mins', NULL),
+  ('4', '2', '2020-01-04 13:53:03', '23.4', '40', NULL),
+  ('5', '3', '2020-01-08 21:10:57', '10', '15', NULL),
+  ('6', '3', 'null', 'null', 'null', 'Restaurant Cancellation'),
+  ('7', '2', '2020-01-08 21:30:45', '25km', '25mins', 'null'),
+  ('8', '2', '2020-01-10 00:15:02', '23.4 km', '15 minute', 'null'),
+  ('9', '2', 'null', 'null', 'null', 'Customer Cancellation'),
+  ('10', '1', '2020-01-11 18:50:20', '10km', '10minutes', 'null');
+
+
+DROP TABLE IF EXISTS pizza_names;
+CREATE TABLE pizza_names (
+  "pizza_id" INTEGER,
+  "pizza_name" TEXT
+);
+INSERT INTO pizza_names
+  ("pizza_id", "pizza_name")
+VALUES
+  (1, 'Meatlovers'),
+  (2, 'Vegetarian');
+
+
+DROP TABLE IF EXISTS pizza_recipes;
+CREATE TABLE pizza_recipes (
+  "pizza_id" INTEGER,
+  "toppings" TEXT
+);
+
+INSERT INTO pizza_recipes
+  ("pizza_id", "toppings")
+VALUES
+  (1, '1, 2, 3, 4, 5, 6, 8, 10'),
+  (2, '4, 6, 7, 9, 11, 12');
+
+
+DROP TABLE IF EXISTS pizza_toppings;
+CREATE TABLE pizza_toppings (
+  "topping_id" INTEGER,
+  "topping_name" TEXT
+);
+INSERT INTO pizza_toppings
+  ("topping_id", "topping_name")
+VALUES
+  (1, 'Bacon'),
+  (2, 'BBQ Sauce'),
+  (3, 'Beef'),
+  (4, 'Cheese'),
+  (5, 'Chicken'),
+  (6, 'Mushrooms'),
+  (7, 'Onions'),
+  (8, 'Pepperoni'),
+  (9, 'Peppers'),
+  (10, 'Salami'),
+  (11, 'Tomatoes'),
+  (12, 'Tomato Sauce');
+
+--Data Cleaning & Transformation
+UPDATE customer_orders
+SET exclusions = NULL WHERE exclusions = 'null' OR exclusions ='';
+
+UPDATE customer_orders
+SET extras = NULL WHERE extras = 'null' OR extras = 'NaN' OR extras ='';
+
+UPDATE runner_orders
+SET cancellation = NULL WHERE cancellation = 'null' OR cancellation = 'NaN' OR cancellation ='';
+
+UPDATE runner_orders
+SET pickup_time = NULL WHERE pickup_time = 'null';
+
+UPDATE runner_orders
+SET distance = NULL WHERE distance = 'null';
+
+UPDATE runner_orders
+SET distance = TRIM('km' from distance) WHERE distance LIKE '%km';
+
+UPDATE runner_orders
+SET duration = NULL WHERE duration = 'null';
+
+UPDATE runner_orders
+SET duration = TRIM('mins' from duration) WHERE duration LIKE '%mins';
+
+UPDATE runner_orders
+SET duration = TRIM('minute' from duration) WHERE duration LIKE '%minute';
+
+UPDATE runner_orders
+SET duration = TRIM('minutes' from duration) WHERE duration LIKE '%minutes';
+
+UPDATE runner_orders
+SET cancellation = NULL WHERE cancellation = 'null' OR cancellation ='' OR cancellation = 'NaN';
+
+--A. PIZZA METRICS
+
+--Query 1 How many pizzas were ordered?
+SELECT COUNT(order_id) AS total_orders
+FROM customer_orders;
+
+--Query 2 How many unique customer orders were made?
+SELECT COUNT(DISTINCT order_id) AS unique_customer
+FROM customer_orders;
+
+-- Query 3 How many successful orders were delivered by each runner?
+SELECT runner_id, COUNT(order_id) AS successful_orders
+FROM runner_orders WHERE distance IS NOT NULL
+GROUP BY runner_id
+ORDER BY runner_id;
+
+-- Query 4 How many of each type of pizza was delivered?
+SELECT c.pizza_id, COUNT (c.order_id) AS delivered
+FROM customer_orders c
+JOIN runner_orders r ON c.order_id=r.order_id
+WHERE r.distance IS NOT NULL
+GROUP BY c.pizza_id
+ORDER BY c.pizza_id; 
+
+--Query 5 How many Vegetarian and Meatlovers were ordered by each customer?
+SELECT c.customer_id, p.pizza_name, COUNT(c.pizza_id) AS order_count
+FROM customer_orders c
+JOIN pizza_names p ON p.pizza_id = c.pizza_id
+GROUP BY c.customer_id, pizza_name
+ORDER BY c.customer_id;
+
+-- Query 6 What was the maximum number of pizzas delivered in a single order?
+WITH CTE AS(
+	SELECT c.order_id, COUNT(c.pizza_id) AS max_pizzas
+	FROM customer_orders c 
+	JOIN runner_orders r ON c.order_id = r.order_id
+	WHERE r.distance IS NOT NULL 
+	GROUP BY c.order_id
+)
+SELECT order_id, max_pizzas
+FROM cte 
+WHERE max_pizzas = (SELECT MAX(max_pizzas) FROM cte)
+
+-- Query 7 For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+SELECT c.customer_id, SUM(
+		CASE WHEN c.exclusions IS NOT NULL OR c.extras IS NOT NULL 
+		THEN 1
+		ELSE 0
+		END
+		) AS atleast_1_change,
+	   SUM(
+		CASE WHEN c.exclusions IS NULL AND c.extras IS NULL 
+		THEN 1
+		ELSE 0
+		END
+		) AS no_change
+FROM customer_orders c
+JOIN runner_orders r ON c.order_id = r.order_id
+WHERE r.distance IS NOT NULL 
+GROUP BY c.customer_id
+ORDER BY c.customer_id;
+
+-- Query 8 How many pizzas were delivered that had both exclusions and extras?
+SELECT SUM(
+	CASE WHEN c.exclusions IS NOT NULL AND c.extras IS NOT NULL THEN 1
+	ELSE 0
+	END
+	) AS altered_pizza
+FROM customer_orders c
+JOIN runner_orders r ON c.order_id = r.order_id
+WHERE r.distance IS NOT NULL;
+
+-- Query 9 What was the total volume of pizzas ordered for each hour of the day?
+SELECT order_id, EXTRACT(HOUR FROM order_time) AS order_hour, COUNT(*) AS total_pizzas
+FROM customer_orders
+GROUP BY order_id, order_hour
+ORDER BY order_id, order_hour;
+
+-- Query 10 What was the volume of orders for each day of the week?
+SELECT order_id, EXTRACT(DAY FROM order_time) AS order_date, COUNT(*) AS total_pizzas
+FROM customer_orders
+GROUP BY order_id, order_date
+ORDER BY order_id, order_date;
+
+-- B. RUNNER AND CUSTOMER EXPERIENCE
+
+-- Query 1 How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
+SELECT TO_CHAR(registration_date, 'W') week_number, COUNT(runner_id)
+FROM runners
+GROUP BY week_number
+ORDER BY week_number;
+
+-- Query 2 What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+SELECT r.runner_id, AVG(EXTRACT(EPOCH FROM(CAST(r.pickup_time AS timestamp) - c.order_time))/60) AS avg_arrival_time
+FROM customer_orders c
+JOIN runner_orders r ON c.order_id = r.order_id
+GROUP BY r.runner_id
+ORDER BY r.runner_id;
+
+-- Query 3 Is there any relationship between the number of pizzas and how long the order takes to prepare?
+SELECT c.order_id, COUNT(c.pizza_id) AS no_of_pizzas, EXTRACT(MINUTE FROM (CAST(r.pickup_time AS timestamp) - c.order_time)) AS preparation_time
+FROM customer_orders c 
+JOIN runner_orders r ON c.order_id = r.order_id
+WHERE pickup_time IS NOT NULL
+GROUP BY c.order_id, preparation_time
+ORDER BY c.order_id;
+
+-- Query 4 What was the average distance travelled for each customer?
+SELECT c.customer_id, ROUND(AVG(CAST(distance AS numeric)), 1) AS avg_distance
+FROM customer_orders c
+JOIN runner_orders r ON c.order_id = r.order_id
+GROUP BY c.customer_id
+ORDER BY c.customer_id;
+
+-- Query 5 What was the difference between the longest and shortest delivery times for all orders?
+SELECT MAX(CAST(duration AS numeric)) - MIN(CAST(duration AS numeric)) AS time_diff
+FROM runner_orders;
+
+-- Query 6 What was the average speed for each runner for each delivery and do you notice any trend for these values?
+SELECT runner_id, order_id, ROUND(AVG(CAST(distance AS numeric) / CAST(duration AS numeric) * 60), 1) AS avg_speed
+FROM runner_orders 
+WHERE distance IS NOT NULL
+GROUP BY runner_id, order_id
+ORDER BY runner_id, order_id;
+
+-- Query 7 What is the successful delivery percentage for each runner?
+SELECT runner_id, COUNT(
+						CASE WHEN distance IS NOT NULL THEN order_id
+						END) *100 / (COUNT(order_id)) AS successful_delivery_percentage
+FROM runner_orders
+GROUP BY runner_id;
