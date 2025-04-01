867651662fb3d9f3e93213049a88f8d16bf07f81
Rupali Jain <rupali.jain@simformsolutions.com>
2025-04-01 18:10:58 +0530

    new update

--- Python/File Handling/Task9.py
+++
@@ -1,8 +0,0 @@
-# Task 9: Copy File Content
-    # 1. Write a Python program to copy the content of students.txt into a new file named
-    # students_backup.txt.
-
-# Copying the content from 'students.txt' file and pasting it in the 'students_backup.txt' file
-with open("students.txt") as source, open("students_backup.txt", "w") as dest: 
-    for line in source:
-        dest.write(line)
